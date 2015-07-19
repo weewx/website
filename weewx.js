f3bf0940e61cda2eda374bcd863eb4e480b6bf7e
Tom Keffer <tkeffer@gmail.com>
2015-07-19 15:15:07 -0700

    Reflect the move from SourceForge to bintray.com

--- weewx.js
+++ weewx.js
@@ -126,7 +126,7 @@ function populate_header(page) {
 <a href='docs.html'>DOCS</a>\
 </div>\
 <div class='navitem'>\
-<a href='https://sourceforge.net/projects/weewx/files/'>DOWNLOAD</a>\
+<a href='https://bintray.com/tkeffer/weewx/Releases/_latestVersion/'>DOWNLOAD</a>\
 </div>";
         tmp = document.createElement('div');
         tmp.setAttribute('class', 'nav');
