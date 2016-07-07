38e8cce792a5da88d4876b40611c9c5c7b3f4c28
Matthew Wall <mwall@picorino.local>
2016-07-07 13:40:09 -0400

    parameterize the rollover.  rollover capture log as well as register log.

--- register/archivelog.pl
+++ register/archivelog.pl
@@ -14,17 +14,24 @@ my $version = '$Id: archivelog.pl 1616 2013-11-05 15:08:42Z mwall $';
 my $basedir = '/home/content/t/o/m/tomkeffer';
 
 # location of the log file
-my $logfn = "$basedir/html/register/register.log";
+my $reglogfn = "$basedir/html/register/register.log";
+my $caplogfn = "$basedir/html/register/capture.log";
 
 # format for filename timestamp
 my $DATE_FORMAT_FN = "%Y%m%d.%H%M%S";
 my $ts = strftime $DATE_FORMAT_FN, gmtime time;
 
-my $oldfn = $logfn;
-my $newfn = "$logfn.$ts";
-
-`mv $oldfn $newfn`;
-`touch $oldfn`;
-`gzip $newfn`;
+rollover($reglogfn);
+rollover($caplogfn);
 
 exit 0;
+
+
+sub rollover {
+    my($logfn) = @_;
+    my $oldfn = $logfn;
+    my $newfn = "$logfn.$ts";
+    `mv $oldfn $newfn`;
+    `touch $oldfn`;
+    `gzip $newfn`;
+}
