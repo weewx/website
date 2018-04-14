7b2ed66929b7d3ba85baf4e0c139302b20a7518c
Tom Keffer <tkeffer@gmail.com>
2018-04-14 07:35:43 -0700

    Added icons for .gz and .rpm. Simplified javascript code.

--- .nginxy/js/config.js
+++ .nginxy/js/config.js
@@ -7,93 +7,153 @@
     Theme author: @lfelipe1501
 \*------------------------------------*/
 // Configure .nginxy here:
-var websiteName = 'weeWX';
-var websiteURL = 'http://www.weewx.com';
+var websiteName = "weeWX";
+var websiteURL = "http://www.weewx.com";
 // End of normal settings.
 //
 //
 
-$(document).ready(function(){
+$(document).ready(function() {
+  // Working on nginx HTML and applying settings.
+  var text = $("#dirname").text();
+  var array = text.split("/");
+  var last = array[array.length - 2];
+  var currentDir = last.charAt(0).toUpperCase() + last.slice(1);
 
-// Working on nginx HTML and applying settings.
-var text = $("#dirname").text();
-var array = text.split('/');
-var last = array[array.length-2];
-var dirStructure = $("a").text();
-dirStructure = document.getElementsByTagName('a')[0].href;
-var dir = text.substring(10);
-var currentDir = last.charAt(0).toUpperCase() + last.slice(1);
-var dirTrun;
+  // Truncate long folder names.
+  if (currentDir.length > 19) {
+    currentDir = currentDir.substring(0, 18) + "...";
+  }
 
-// Truncate long folder names.
-if (currentDir.length > 19){
-	currentDir = currentDir.substring(0, 18) + '...';
-}
+  // Updating page title.
+  document.title = websiteName + ": " + currentDir;
 
-// Updating page title.
-document.title = websiteName + ': ' + currentDir;
+  $("#dirname").html(currentDir);
 
-// Updating page footer.
-$("#footerURL").text(websiteName);
-$("#footerURL").attr('href', websiteURL);
+  // Establish supported formats.
+  var formats = [
+    "7z",
+    "avi",
+    "bat",
+    "bin",
+    "bmp",
+    "c",
+    "c++",
+    "cmd",
+    "css",
+    "deb",
+    "doc",
+    "docx",
+    "exe",
+    "gif",
+    "gz",
+    "gzip",
+    "html",
+    "ico",
+    "iso",
+    "java",
+    "jpeg",
+    "jpg",
+    "js",
+    "mp3",
+    "mp4",
+    "msg",
+    "ogg",
+    "pdf",
+    "php",
+    "png",
+    "ppt",
+    "pptx",
+    "psd",
+    "py",
+    "rar",
+    "raw",
+    "rpm",
+    "sh",
+    "sql",
+    "svg",
+    "swf",
+    "tiff",
+    "torrent",
+    "txt",
+    "wav",
+    "wma",
+    "wmv",
+    "xls",
+    "xlsx",
+    "zip"
+  ];
 
-// Add back button.
-$("#dirname").html(currentDir);
+  // Scan all files in the directory, check the extensions and show the right MIME-type image.
+  $("td a").each(function() {
+    var found = 0;
+    var arraySplit = $(this)
+      .attr("href")
+      .split(".");
+    var fileExt = arraySplit[arraySplit.length - 1];
+    var oldText;
 
-if (dir.length > 60) {
-	dirTrun = dir.replace(/(.{60})/g, "$1\n")
-} else {
-	dirTrun = dir.substring(0, dir.length - 1);
-}
+    for (var i = 0; i < formats.length; i++) {
+      if (fileExt.toLowerCase() === formats[i].toLowerCase()) {
+        found = 1;
+        oldText = $(this).text();
+        $(this).html(
+          '<img class="icons" src="/.nginxy/images/icons/' +
+            formats[i] +
+            '.png" style="margin:0 4px -4px 0"/></a>' +
+            oldText
+        );
+        return;
+      }
+    }
 
-// Establish supported formats.
-var list = new Array();
-var formats = ["bin", "jpg", "gif", "bmp", "png", "html", "css", "zip", "iso", "tiff", "ico", "psd", "pdf", "exe", "rar", "deb", "swf", "7z", "doc", "docx", "xls", "xlsx", "pptx", "ppt", "txt", "php", "js", "c", "c++", "torrent", "sql", "wmv", "avi", "mp4", "mp3", "wma", "ogg", "msg", "wav", "py", "java", "gzip", "jpeg", "raw", "cmd", "bat", "sh", "svg"];
+    // Add an icon for the go-back link.
+    if (
+      $(this)
+        .text()
+        .indexOf("Parent directory") >= 0
+    ) {
+      found = 1;
+      oldText = $(this).text();
+      $(this).html(
+        '<img class="icons" src="/.nginxy/images/icons/home.png" ' +
+          'style="margin:0 4px -4px 0"/>' +
+          oldText
+      );
+      return;
+    }
 
-// Scan all files in the directory, check the extensions and show the right MIME-type image.
-$('td a').each(function(){
-	var found = 0;
-	var arraySplit = $(this).attr('href').split(".");
-	var fileExt = arraySplit[arraySplit.length - 1];
+    // Check for folders as they don't have extensions.
+    if (
+      $(this)
+        .attr("href")
+        .substr($(this).attr("href").length - 1) === "/"
+    ) {
+      found = 1;
+      oldText = $(this).text();
+      $(this).html(
+        '<img class="icons" src="/.nginxy/images/icons/folder.png" ' +
+          'style="margin:0 4px -4px 0"/>' +
+          oldText.substring(0, oldText.length - 1)
+      );
 
-	for (var i = 0; i < formats.length; i++) {
-		if (fileExt.toLowerCase() == formats[i].toLowerCase()) {
-			var found = 1;
-			var oldText = $(this).text();
-			$(this).html('<img class="icons" src="/.nginxy/images/icons/' + formats[i] + '.png" style="margin:0px 4px -4px 0px"></img></a>' + oldText);
-			return;
-		}
-	}
+      // Fix for annoying jQuery behaviour where inserted spaces are treated as new elements -- which breaks my search.
+      var string = " " + $($(this)[0].nextSibling).text();
 
-	// Add an icon for the go-back link.
-	if ($(this).text().indexOf("Parent directory") >= 0) {
-		var found = 1;
-		var oldText = $(this).text();
-		$(this).html('<img class="icons" src="/.nginxy/images/icons/home.png" style="margin:0px 4px -4px 0px">' + oldText);
-		return;
-	}
+      // Copy the original meta-data string, append a space char and save it over the old string.
+      $($(this)[0].nextSibling).remove();
+      $(this).after(string);
+      return;
+    }
 
-
-	// Check for folders as they don't have extensions.
-	if ($(this).attr('href').substr($(this).attr('href').length - 1) == '/') {
-		var found = 1;
-		var oldText = $(this).text();
-		$(this).html('<img class="icons" src="/.nginxy/images/icons/folder.png" style="margin:0px 4px -4px 0px">' + oldText.substring(0, oldText.length - 1));
-
-		// Fix for annoying jQuery behaviour where inserted spaces are treated as new elements -- which breaks my search.
-		var string = ' ' + $($(this)[0].nextSibling).text();
-
-		// Copy the original meta-data string, append a space char and save it over the old string.
-		$($(this)[0].nextSibling).remove();
-		$(this).after(string);
-		return;
-	}
-
-	// File format not supported by Better Listings, so let's load a generic icon.
-	if (found == 0){
-		var oldText = $(this).text();
-		$(this).html('<img class="icons" src="/.nginxy/images/icons/error.png" style="margin:0px 4px -4px 0px">' + oldText);
-		return;
-	}
-});
+    // File format not supported by Better Listings, so let's load a generic icon.
+    if (found === 0) {
+      oldText = $(this).text();
+      $(this).html(
+        '<img class="icons" src="/.nginxy/images/icons/error.png" ' +
+          'style="margin:0 4px -4px 0"/>' +
+          oldText
+      );
+    }
+  });
 });
