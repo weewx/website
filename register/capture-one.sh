52c94c9def4892cf1141c0d13292f2308896714d
Matthew Wall <matthewwall@users.noreply.github.com>
2023-04-10 08:45:48 -0400

    added script to capture single web site

---
+++ register/capture-one.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# capture a single station based on its url
+#
+# usage:
+#   capture-one.sh URL
+
+station_url=$1
+
+# start an X display
+Xvfb :100 -screen 0 1024x760x24 &
+pid=$!
+# do the capture using the display we just set up
+DISPLAY=:100 /var/www/html/register/capture.pl --url $station_url
+# kill the X display
+kill $pid
